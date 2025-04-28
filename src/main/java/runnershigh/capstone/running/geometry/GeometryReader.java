0c33560fdb899348316c113cba661ce081a923de
songhaechan <bukak2019@naver.com>
2025-04-28 22:21:04 +0900

    fix: geojson read 오류

--- src/main/java/runnershigh/capstone/running/geometry/GeometryReader.java
+++ src/main/java/runnershigh/capstone/running/geometry/GeometryReader.java
@@ -3,6 +3,7 @@ package runnershigh.capstone.running.geometry;
 import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
@@ -11,14 +12,17 @@ import org.locationtech.jts.io.geojson.GeoJsonReader;
 import runnershigh.capstone.global.error.ErrorCode;
 import runnershigh.capstone.running.exception.GeometryParseException;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GeometryReader {
 
     public static Geometry readFrom(final Document courseDocument){
         Geometry geometry = null;
         try{
-            geometry = new GeoJsonReader(new GeometryFactory()).read(courseDocument.toJson());
+            Document geometryJson = (Document)courseDocument.get("geometry");
+            geometry = new GeoJsonReader(new GeometryFactory()).read(geometryJson.toJson());
         }catch (ParseException e){
+            log.error(e.getLocalizedMessage());
             throw new GeometryParseException(ErrorCode.COULD_NOT_PARSE_GEOMETRY);
         }
         return geometry;
