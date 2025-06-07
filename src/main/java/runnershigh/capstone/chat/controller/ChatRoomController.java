1b3ae6d28c758cabc7cfcf5683592c3658d77022
jjinbok <haba8730@naver.com>
2025-06-08 01:24:38 +0900

    feat : chatController swagger 추가

--- src/main/java/runnershigh/capstone/chat/controller/ChatRoomController.java
+++ src/main/java/runnershigh/capstone/chat/controller/ChatRoomController.java
@@ -1,6 +1,8 @@
 package runnershigh.capstone.chat.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +16,20 @@ import runnershigh.capstone.global.argumentresolver.AuthUser;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/chatroom")
+@Tag(name = "채팅방 [채팅방 조회]")
 public class ChatRoomController {
 
     private final ChatService chatService;
 
     @GetMapping("/all")
+    @Operation(summary = "유저 채팅방 전체 조회", description = "유저 ID를 받아, 유저가 참여되어있는 채팅방 전체 조회.")
     public ChatRoomPreviewListResponse getChatRoomPreviews(
         @Parameter(hidden = true) @AuthUser Long userId) {
         return chatService.getChatRoomPreviews(userId);
     }
 
     @GetMapping("/{roomId}")
+    @Operation(summary = "채팅방 내역 조회", description = "채팅방 ID를 받아, 채팅방의 내역 조회.")
     public ChatMessageListResponse getChatMessages(@PathVariable Long roomId) {
         return chatService.getChatMessages(roomId);
     }
