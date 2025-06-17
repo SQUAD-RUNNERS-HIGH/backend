985394f30cbed3a6acf95c4733053249b0a691e6
songhaechan <bukak2019@naver.com>
2025-06-17 20:42:46 +0900

    fix: StompInterceptor

--- src/main/java/runnershigh/capstone/global/config/stomp/StompInterceptor.java
+++ src/main/java/runnershigh/capstone/global/config/stomp/StompInterceptor.java
@@ -25,39 +25,44 @@ public class StompInterceptor implements ChannelInterceptor {
     private final JwtValidator jwtValidator;
     private final JwtExtractor jwtExtractor;
 
-    private boolean isDestinationUserQueue(final StompHeaderAccessor accessor) {
-        return accessor.getDestination().startsWith("/user/queue");
-    }
-
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         log.info("COMMAND : {} ", accessor.getCommand());
-
+        Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            String authHeader = accessor.getFirstNativeHeader(NATIVE_AUTHORIZATION_HEADER);
-            if (authHeader.startsWith(BEARER_PREFIX)) {
-                String token = authHeader.substring(7);
-                validateToken(token, accessor);
-            } else {
-                throw new JwtNotFoundException(ErrorCode.INVALID_TOKEN);
-            }
-            Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
-            String endpoint = (String) sessionAttributes.get("endpoint");
-            if (Objects.nonNull(endpoint) && endpoint.equals("running")) {
-                String crewId = accessor.getFirstNativeHeader("CrewId");
-                String courseId = accessor.getFirstNativeHeader("CourseId");
-                sessionAttributes.put("crewId",crewId);
-                sessionAttributes.put("courseId",courseId);
-            }
+            setUserIdToSession(accessor, sessionAttributes);
+            setCrewRunningToSession(sessionAttributes, accessor);
         }
         return message;
     }
 
-    private void validateToken(final String token, final StompHeaderAccessor accessor) {
+    private void setUserIdToSession(final StompHeaderAccessor accessor, final Map<String, Object> sessionAttributes) {
+        String authHeader = accessor.getFirstNativeHeader(NATIVE_AUTHORIZATION_HEADER);
+        if (authHeader.startsWith(BEARER_PREFIX)) {
+            String token = authHeader.substring(7);
+            setUserId(token, sessionAttributes);
+        } else {
+            throw new JwtNotFoundException(ErrorCode.INVALID_TOKEN);
+        }
+    }
+
+    private void setCrewRunningToSession(final Map<String, Object> sessionAttributes,
+        final StompHeaderAccessor accessor) {
+        String endpoint = (String) sessionAttributes.get("endpoint");
+        boolean isCrewRun = Boolean.parseBoolean(accessor.getFirstNativeHeader("CrewRun"));
+        if (Objects.nonNull(endpoint) && endpoint.equals("running") && isCrewRun) {
+            String crewId = accessor.getFirstNativeHeader("CrewId");
+            String courseId = accessor.getFirstNativeHeader("CourseId");
+            sessionAttributes.put("crewId",crewId);
+            sessionAttributes.put("courseId",courseId);
+        }
+    }
+
+    private void setUserId(final String token, final Map<String,Object> sessionAttributes) {
         if (jwtValidator.validateAccessToken(token)) {
             Long userId = jwtExtractor.extractUserIdByAccessToken(token);
-            accessor.getSessionAttributes().put("userId", userId);
+            sessionAttributes.put("userId", userId);
         } else {
             throw new JwtNotFoundException(ErrorCode.INVALID_TOKEN);
         }
